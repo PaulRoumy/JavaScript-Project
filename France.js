fcde13e94cd598c33d2125de927efe97b1471cdf
Paul Roumy <paul.roumy@edu.itescia.fr>
2020-11-03 15:23:34 +0100

    Debut Js

---
+++ France.js
@@ -0,0 +1,19 @@
+let Infos =document.getElementById('Infos')
+Infos.addEventListener('click',affInfo)
+function affInfo(){
+    let n = 0
+    n+=1
+    console.log(n);
+}
+let resume =document.getElementById('Resume')
+resume.addEventListener('click',affRes)
+function affRes (){
+    let Re =2
+    console.log(Re)
+}
+let Place =document.getElementById('Place')
+Place.addEventListener('click',affPla)
+function affPla(){
+    let Pla = 3
+    console.log(Pla)
+}
