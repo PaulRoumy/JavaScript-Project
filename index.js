c287fad4e0155e7d432fe6f61cd92d293620c0b7
ClaireW <claira.m@live.fr>
2020-11-04 13:34:15 +0100

    exo recup valeur et afficage plus haut et boucler dans un tableau

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
-let numb = 0;
+// Lignes 3 à 44 apparition et disparition des drapeaux au survol,
+// j'ai voulu le faire de 2 façons différentes et je m'aperçois 
+// que la 2ème (pour le drapeau du brésil) est plus nette sur le rendu
+
+let frenchbtn = document.getElementById('french')
+
+frenchbtn.addEventListener('mouseover', franceover)
+frenchbtn.addEventListener('mouseout', francedisappear)
+
+let frenchf = document.getElementById('frenchf')
+function franceover (e) {
+frenchf.removeAttribute('class')
+
+}
+function francedisappear (e) {
+frenchf.setAttribute('class', 'invisible')
+}
+
+let italianbtn = document.getElementById('italian')
+
+italianbtn.addEventListener('mouseover', italyover)
+italianbtn.addEventListener('mouseout', italydisappear)
+
+let italyf = document.getElementById('italianf')
+function italyover (e) {
+   italianf.removeAttribute('class') 
+}
+function italydisappear (e) {
+    italianf.setAttribute('class', 'invisible')
+}
+
+let brasilbtn = document.getElementById('brasil')
+brasilbtn.addEventListener('mouseover', brasilover)
+brasilbtn.addEventListener('mouseout', brasildisappear)
+
+let brasilf = document.getElementById('brasilianf')
+function brasilover (e) {
+//  brasilf.removeAttribute('class')
+brasilf.style.display='block'
+}
+function brasildisappear (e) {
+    brasilf.style.display='none'
+}
+
+
+// Afficher le texte de l'input
+// let input = document.getElementById("inputTxt")
+// let inputValue = input.value;
+// console.log("la valeur de l'input est : " + inputValue)
+let mySendBtn = document.getElementById('mySendBtn');
+mySendBtn.addEventListener('click', displayTxt)
+
+
+function displayTxt (e) {
+    // Je récupère la valeur de mon input et le stocke dans une variable
+    let inputValue = document.getElementById("inputTxt").value
+ 
+    // je l'affiche dans ma console
+  console.log("ceci est la valeur de l'input texte : " + inputValue); 
+  
+//   // je l'affiche dans le html :
+//     let paragraph = document.getElementsByClassName("textaremplir")
+//     console.log(paragraph)
+//     let textDuP = paragraph.innerText = inputValue;
+
+let div = document.getElementsByTagName('div')[0]
+console.log(div)
+
+let newH2 = document.createElement('h2')
+newH2.className = 'myH2'
+newH2.textContent = 'Bonjour ' + inputValue
+div.appendChild(newH2);
+console.log(div)
+console.log(newH2)
+}
+// Je crée un tableau : 
+let countriesArray = ['Japon', 'Canada', 'Thaïlande', 'Chili', 'Norvège', 'Islande', 'Portugal'];
+let div4 = document.getElementsByTagName('div')[3]
+    let ul = document.createElement('ul')
+    div4.appendChild(ul)
+    // Je boucle dans mon tableau :
+for (let i = 0; i < countriesArray.length; i++) {
+    let li = document.createElement('li')
+    li.textContent = countriesArray[i]
+    ul.appendChild(li);
+
+    // console.log(countriesArray[i])
+}
